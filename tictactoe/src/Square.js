ace30ebb05768e1abdbc877a1c4879ad059fc7fc
charan <sreecharan990@gmail.com>
2019-07-29 14:35:16 -0500

    Implemented Square constant

--- tictactoe/src/Square.js
+++ tictactoe/src/Square.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Square = props =>{}
-    
+const Square = props =>(
+    <button className={`${props.winnerClass} square`} onClick={props.onClick}>
+      {props.value}
+    </button>
+); 
   
-
 export default Square;
