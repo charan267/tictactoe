2145fcaeb10069919dc345f876eb17af8b9aa4b0
charan <sreecharan990@gmail.com>
2019-07-29 12:47:29 -0500

    Implemented Component

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -1,1 +1,19 @@
 import React from 'react'
+
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Board;
--- tictactoe/src/Square.js
+++ tictactoe/src/Square.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+class Square extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Square;
