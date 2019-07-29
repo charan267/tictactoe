38ce701f54d3c549eb1faa86bbff2dbe0aac459c
charan <sreecharan990@gmail.com>
2019-07-29 14:16:04 -0500

    Implemented board function

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -3,7 +3,16 @@ import React from 'react'
 
 class Board extends React.Component{
     createBoard(row, col) {
-        
+        const board = [];
+    let cellCounter = 0;
+
+    for (let i = 0; i < row; i += 1) {
+      const columns = [];
+      for (let j = 0; j < col; j += 1) {
+        columns.push(this.renderSquare(cellCounter++));
+      }
+      board.push(<div key={i} className="board-row">{columns}</div>);
+    }
         return board;
       }
     
