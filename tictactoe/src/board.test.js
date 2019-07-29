9fbdb72a42affd86985dee5b11923e03579bd134
charan <sreecharan990@gmail.com>
2019-07-29 15:03:32 -0500

    Implemented a function to render without crashing using shallow

--- tictactoe/src/board.test.js
+++ tictactoe/src/board.test.js
@@ -2,5 +2,6 @@ import React from 'react'
 import Board from './Board'
 import {shallow} from 'enzyme'
 it('renders without crashing', () => {
-   
+    let squares = Array(9).fill(null)
+    shallow(<Board squares={squares}/>);
   });
